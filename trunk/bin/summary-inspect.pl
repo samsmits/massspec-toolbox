b3a32cf2d035bcdf47e832178f9625f6f1ea6db2
linusben.utexas <linusben.utexas@6f3b5e19-fd54-0410-8e82-a90e9395dd2e>
2008-09-07 14:46:40 +0000

    - Add scripts for output summary.

---
+++ trunk/bin/summary-inspect.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+use Cwd;
+use File::Spec;
+use strict;
+use warnings;
+
+my $current_dir = getcwd();
+my @parent_dirs = File::Spec->splitdir($current_dir);
+my $project_name = pop(@parent_dirs);
+my $file_output = $project_name.'.inspect.summary';
+
+my %spectra;
+foreach my $file_inspect (`ls inspect/*.out`) {
+  chomp($file_inspect);
+  print STDERR "Read $file_inspect ... ";
+  open(INSP,$file_inspect);
+  while(<INSP>) {
+    if(/^#/) { next; }
+    chomp;
+    my ($mzxml, $scan, $peptide, $annotation, $charge, @tmp) = split(/\t/);
+    my $sample = 'unknown';
+    if( $mzxml =~ /([A-z0-9\_]+)\.mzXML$/ ) { 
+      $sample = $1;
+    }
+    $peptide =~ s/^[A-Z]\.//;
+    $peptide =~ s/\.[A-Z]$//;
+    $scan .= '.'.$charge;
+
+    $spectra{$sample}->{$scan}->{$peptide} = $tmp[8];
+
+    if( $sample eq 'unknown' ) {
+      die "Unknown sample : $mzxml\n";
+    }
+  }
+  close(INSP);
+  print STDERR "Done\n";
+}
+
+foreach my $file_inspect (`ls inspect.PValue/*.out`) {
+  chomp($file_inspect);
+  print STDERR "Read $file_inspect ... ";
+  open(INSP,$file_inspect);
+  while(<INSP>) {
+    if(/^#/) { next; }
+    chomp;
+    my ($mzxml, $scan, $peptide, $annotation, $charge, @tmp) = split(/\t/);
+    my $sample = 'unknown';
+    if( $mzxml =~ /([A-z0-9\_]+)\.mzXML$/ ) { 
+      $sample = $1;
+    }
+    $peptide =~ s/^[A-Z]\.//;
+    $peptide =~ s/\.[A-Z]$//;
+    $scan .= '.'.$charge;
+    $spectra{$sample}->{$scan}->{$peptide} = $tmp[8];
+
+    if( $sample eq 'unknown' ) {
+      die "Unknown sample : $mzxml\n";
+    }
+  }
+  close(INSP);
+  print STDERR "Done\n";
+}
+
+open(OUTPUT,">$file_output");
+print STDERR "Write $file_output ... \n";
+foreach my $sample (sort keys %spectra) {
+  foreach my $scan (sort keys %{$spectra{$sample}}) {
+    foreach my $peptide (sort keys %{$spectra{$sample}->{$scan}}) {
+      print OUTPUT join("\t",$sample,$scan,$peptide,
+                $spectra{$sample}->{$scan}->{$peptide}),"\n";
+      if( $spectra{$sample}->{$scan}->{$peptide} <= 0.05 ) {
+        print join("\t",$sample,$scan,$peptide),"\n";
+      }
+    }
+  }
+}
+close(OUTPUT);
