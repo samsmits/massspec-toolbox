6d29f905541a6b911347146637251c1e4b9aeb13
linusben.utexas <linusben.utexas@6f3b5e19-fd54-0410-8e82-a90e9395dd2e>
2008-09-07 14:53:18 +0000

    - Add scripts to rename tandem output + convert to pepxml - Add script to prepare TPP for tandem output with k-score.

---
+++ trunk/bin/rename-tandem-out.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+foreach my $file_old (`ls tandem/*.out`) {
+  chomp($file_old );
+  my $file_new = $file_old;
+  $file_new =~ s/[0-9\_]+\.t\.//;
+  if( $file_old eq $file_new ) {
+    print STDERR "Skip $file_old \n";
+  } else {
+    print STDERR "$file_old --> $file_new\n";
+    `mv $file_old $file_new`;
+  }
+  my $file_pepxml = $file_new;
+  $file_pepxml =~ s/out$/pepxml/;
+  `/usr/local/tpp/bin/Tandem2XML $file_new > $file_pepxml`;
+}
