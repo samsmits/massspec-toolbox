e2ec588bad6ec485ae88f2a1157f302cdef03790
linusben.utexas <linusben.utexas@6f3b5e19-fd54-0410-8e82-a90e9395dd2e>
2008-09-11 19:59:05 +0000

    - More flexible structures (with DB file in each directory) - Incorporate with APEX scripts.

--- bin/make-mgf.sh
+++ bin/make-mgf.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050422/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050423/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050424/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050428/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050430/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050527/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050531/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050601/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050614/mzXML/*.mzXML
-/usr/local/tpp/bin/MzXML2Search -mgf ./YEAST_20050615/mzXML/*.mzXML
+/usr/local/tpp/bin/MzXML2Search -mgf ./mzXML/*.mzXML
+mv ./mzXML/*.mgf ./mgf/
