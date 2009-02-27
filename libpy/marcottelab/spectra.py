bfebe9e5a23b5c20040cf1c08635e76b49698d34
linusben.utexas <linusben.utexas@6f3b5e19-fd54-0410-8e82-a90e9395dd2e>
2009-02-27 19:23:10 +0000

    - Add compress/uncompress method in marcottelab.spectra

--- libpy/marcottelab/spectra.py
+++ libpy/marcottelab/spectra.py
@@ -1,3 +1,6 @@
+import struct
+import base64
+
 class MS1:
   def __init__(self):
     self.id = 0
@@ -12,4 +15,12 @@ class MS2:
     self.charge = 0
     self.precursor_mass = 0
     self.SearchHit_list = []
-  
+
+def encode_double_list(double_list):
+  packed = ''
+  for tmp in double_list:
+    packed += struct.pack('d',tmp)
+  return base64.b64encode(packed)
+
+def decode_double_list(count, encoded_string):
+  return struct.unpack("%dd"%count, base64.b64decode(encoded_string))
