6d3534a1b8c062c5a73f2a9c8da03e7040352cc5
linusben.utexas <linusben.utexas@6f3b5e19-fd54-0410-8e82-a90e9395dd2e>
2010-04-30 21:51:04 +0000

    - Two versions of mzxml2precursor_list: with or without expat

--- mzxml/mzxml2precursor_list.py
+++ mzxml/mzxml2precursor_list.py
@@ -1,32 +1,56 @@
 #!/usr/bin/python
-import sys
 import os
+import sys
+import re
+import gzip
 
-from MzXML import MzXML
-
-usage_mesg = 'Usage: mzxml2precursor_list.py <mzXML file>'
+usage_mesg = 'Usage: extract-precursor-intensity.py <.mzXML file>'
 
 if( len(sys.argv) != 2 ):
     print usage_mesg
     sys.exit(1)
 
-def check_file(filename):
-    if( not os.access(filename,os.R_OK) ):
-        print "%s is not accessible."%filename
-        print usage_mesg
-        sys.exit(1)
-
 filename_mzXML = sys.argv[1]
-check_file(filename_mzXML)
+if( not os.access(filename_mzXML,os.R_OK) ):
+    print usage_mesg
+    sys.exit(1)
 
-ms1_mz2i = dict()
-mzXML = MzXML()
-mzXML.parse_file(filename_mzXML)
+filename_base = os.path.basename(filename_mzXML).replace('.mzXML','').replace('.gz','')
 
-f_out = open(filename_mzXML+'.precursor_list','w')
-f_out.write("#Scan_ID\tPrecursor_Mz\tPrecursor_Intensity\tRetentionTime\n")
-for tmp_ms2 in mzXML.MS2_list:
-    scan_id = tmp_ms2.id
-    #print scan_id,tmp_ms2.precursor_intensity, tmp_ms2.precursor_mz
-    f_out.write("%d\t%.5f\t%.2f\t%.3f\n"%(scan_id,tmp_ms2.precursor_mz,tmp_ms2.precursor_intensity,tmp_ms2.retention_time))
+scan_num = 0
+ms_level = 0
+ret_time = 0.0
+re_scan_num = re.compile('<scan num="([0-9]+)"')
+re_retention_time = re.compile('retentionTime="PT([0-9\.]+)S"')
+re_precursor_mz = re.compile('([0-9\.e\+]+)</precursorMz>')
+re_precursor_intensity = re.compile('precursorIntensity="([0-9\.e\+]+)"')
+re_precursor_charge = re.compile('precursorCharge="([0-9]+)"')
+
+f_out = open(filename_base+'.precursor_list','w')
+
+sys.stderr.write("Write %s.precursor_list ... "%filename_base)
+f_out.write("#Scan_id\tPrecursor_Mz\tPrecursor_Intensity\tRetentionTime\n")
+f_mzXML = open(filename_mzXML,'r')
+if( filename_mzXML.endswith('.gz') ):
+    f_mzXML = gzip.open(filename_mzXML,'rb')
+
+for line in f_mzXML:
+    line = line.strip()
+    if( re_scan_num.search(line) >= 0 ):
+        scan_num = re_scan_num.search(line).group(1)
+    if( line == 'msLevel="2"' ):
+        ms_level = '2'
+    elif( line.find('msLevel=') >= 0 ):
+        ms_level = '0'
+    if( re_retention_time.search(line) >= 0 ):
+        ret_time = re_retention_time.search(line).group(1)
+    if( ms_level == '2' ):
+        if( line.startswith('<precursorMz ') ):
+            intensity = re_precursor_intensity.search(line).group(1)
+            charge = re_precursor_charge.search(line).group(1)
+            mz = re_precursor_mz.search(line).group(1)
+            scan_id = "%s.%s.%s.%s"%(filename_base,scan_num,scan_num,charge)
+            f_out.write("%s\t%s\t%s\t%s\n"%(scan_id,mz,intensity,ret_time))
+f_mzXML.close()
 f_out.close()
+sys.stderr.write("Done\n")
